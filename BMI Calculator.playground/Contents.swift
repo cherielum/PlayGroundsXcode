4f1f93994b64e21a41993396f0b1d62a03e22787
Cherie Lum <charismatic.cherie@gmail.com>
2018-03-13 13:55:20 -0400

    american version

--- BMI Calculator.playground/Contents.swift
+++ BMI Calculator.playground/Contents.swift
@@ -49,26 +49,42 @@ func BMICalculator(weight : Double, height : Double ) -> Double {
 //}
 
 //even more accurate
-func BMICalculator(weight : Double, height : Double ) -> String {
-    
-    let BMI = weight/pow(height, 2)
-    let shortenedBMI = String(format: "%.2f", BMI)
-    var interpretation = ""
-    
-    if BMI > 25 {
-        interpretation = "You are overweight."
-    } else if BMI > 18.5 && BMI <= 25 {
-        interpretation = "You are of normal weight."
-    } else {
-        interpretation = "You are underweight."
-    }
-    return "Your BMI is \(shortenedBMI) and \(interpretation) "
-}
-
+//func BMICalculator(weight : Double, height : Double ) -> String {
+//
+//    let BMI = weight/pow(height, 2)
+//    let shortenedBMI = String(format: "%.2f", BMI)
+//    var interpretation = ""
+//
+//    if BMI > 25 {
+//        interpretation = "You are overweight."
+//    } else if BMI > 18.5 && BMI <= 25 {
+//        interpretation = "You are of normal weight."
+//    } else {
+//        interpretation = "You are underweight."
+//    }
+//    return "Your BMI is \(shortenedBMI) and \(interpretation) "
+//}
 
 //calling function
-print(BMICalculator(weight: 140, height: 1.72))
+//print(BMICalculator(weight: 140, height: 1.72))
 
 //another way
 var BMIResult = BMICalculator(weight: 140, height: 1.72)
 print(BMIResult)
+
+
+//American Version
+func BMICalcImperialUnits(weightInPounds: Double, heightInFeet: Double, heightInInches: Double) -> String {
+    
+    let weightInKg = weightInPounds * 0.4539237
+    let totalInches = heightInFeet * 12 + heightInInches
+    let heightInM = totalInches * 0.0254
+    
+    let BMI = weightInKg / pow(heightInM, 2)
+    //let BMI = weightInKg / (heightInM * heightInM)
+    
+    return "Your BMI is \(BMI)."
+}
+
+var calcAmericanNumbers = BMICalcImperialUnits(weightInPounds: 140, heightInFeet: 5, heightInInches: 8)
+print(calcAmericanNumbers)
